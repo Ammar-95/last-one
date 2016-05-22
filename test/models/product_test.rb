6929287355f3f002bc3e2e97b81a68da5a94a2bd
Joseph Finlayson <joseph.finlayson@gmail.com>
2016-05-22 14:46:57 +0200

    add tests to product

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,40 @@
 require 'test_helper'
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "product attributes must not be empty" do
+    product = Product.new
+
+    assert product.invalid?
+    assert product.errors.any?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+  end
+
+  test "product with all fields set should be valid" do
+
+    product = Product.new({
+                            title: 'string',
+                            price: 10.0,
+                            description: 'some text here',
+                            image_url: 'http://asdasd.com/a.jpg'
+    })
+
+    assert product.valid?
+
+  end
+
+  test "product with a incorrect image_url is not valid" do
+    product = Product.new(
+      {
+        title: 'string',
+        price: 10.0,
+        description: 'some text here',
+        image_url: 'http://asdasd.com/crazy'
+    })
+    assert product.invalid?
+    assert product.errors[:image_url].any?
+  end
 end
